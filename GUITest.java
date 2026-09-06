a6f73f971420ca5a43348bdfba285e38976db7cd
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-09-06 22:17:44 +0500

    Update GUITest.java

--- GUITest.java
+++ GUITest.java
@@ -1,4 +1,3 @@
-
 package guitest;
 
 import javax.swing.*;
@@ -7,41 +6,57 @@ import java.awt.*;
 
 import java.awt.event.*;
 
-
 public class GUITest {
 
-    
     public static void main(String[] args) {
+        
         JFrame jf = new JFrame("My First Window");
+        
         jf.setLayout(new java.awt.GridLayout(3,2));
+        
         JTextField text = new JTextField(20);
+        
         JButton jbok = new JButton("OK");
+        
         JButton jbcancel = new JButton("Cancel");
         
-        jbok.addActionListener(new ActionListener(){            
+        jbok.addActionListener(new ActionListener(){    
+            
            public void actionPerformed(ActionEvent e){
           
                text.setText("you clicked ok");
-               //System.out.println("Okay button clicked");            
-             //   JOptionPane.showMessageDialog(null,'');
-            }          
-        }); 
+             
+            } 
+            
+        }
+                               
+    ); 
         
-         jbcancel.addActionListener(new ActionListener(){            
+         jbcancel.addActionListener(new ActionListener(){     
+             
            public void actionPerformed(ActionEvent e){
                
                text.setText("you clicked cancel");
         
-       
-            
             System.out.println("Cancel button clicked");
+               
         }
-    });
+             
+    }
+                                    
+);
+        
         jf.add(text);
+        
         jf.add(jbok);
+        
         jf.add(jbcancel);
+        
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         jf.setSize(400,300);
+        
         jf.setVisible(true);
+        
     }
 }
